07f35b765d316b4fed2cebac6d0d4713d437302b
dongdongGit <1697919363@qq.com>
2019-11-14 13:58:49 +0800

    fix

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,16 +7,18 @@ class Task extends Model
     const UPDATED_AT = null;
 
     protected $casts = [
+        'onedrive_id'  => 'integer',
         'completed_at' => 'datetime',
         'failed_at'    => 'datetime'
     ];
 
     protected $columns = [
         'id',
-        'onedrive_id',
         'gid',
         'status',
-        'path',
+        'onedrive_id',
+        'source',
+        'target',
         'created_at',
         'completed_at',
         'failed_at'
